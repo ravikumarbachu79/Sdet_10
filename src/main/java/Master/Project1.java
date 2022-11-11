f52e97cf38c168867af969386d5da718fb05654d
ravik <ravik@DESKTOP-93PTIUJ>
2022-11-11 12:05:23 +0530

    push by master

---
+++ src/main/java/Master/Project1.java
@@ -0,0 +1,9 @@
+package Master;
+
+public class Project1 {
+
+	public static void main(String[] args) {
+	System.out.println("project1");
+	}
+
+}
---
+++ src/main/java/Master/Project2.java
@@ -0,0 +1,9 @@
+package Master;
+
+public class Project2 {
+
+	public static void main(String[] args) {
+		System.out.println("project2");
+	}
+
+}
